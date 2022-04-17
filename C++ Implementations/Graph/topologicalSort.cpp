313ebbf2941007c0857914ff24b6e56c6106874b
Akash Mishra <akashmishra.akm@gmail.com>
2022-04-17 21:59:40 +0530

    Implemented with dfs

--- C++ Implementations/Graph/topologicalSort.cpp
+++ C++ Implementations/Graph/topologicalSort.cpp
@@ -47,25 +47,57 @@ public:
 
       }
       for(int i:result){
-          cout<<i<<"  -> ";
+          cout<<i<<" -> ";
       }
     }
      
+     void topologicalSort_DFS(){
+         stack<int> st;
+         for(int i=0;i<vertices;i++){             
+                if(!vis[i]){
+                    dfs(i,st);
+                }             
+       }
+       while(!st.empty()){
+           cout<<st.top()<<" -> ";
+           st.pop();
+       }
+     }
+
+     void dfs(int node,stack<int> &st){
+        vis[node]=1;
+        for(auto neigh:adj[node]){
+            if(!vis[neigh]){
+                dfs(neigh,st);
+            }
+        }
+        st.push(node);
+     }
 };
 
 int main(){
-    Graph g(6);
-    g.addEdge(0,1);
-    g.addEdge(0,3);
-    g.addEdge(1,2);
-    g.addEdge(1,3);
-    g.addEdge(2,3);
-    g.addEdge(2,4);
-    g.addEdge(3,4);
-    g.addEdge(3,5);
-    g.addEdge(4,5);
-
-    g.topologicalSort_BFS();
+    // Graph g(6);
+    // g.addEdge(0,1);
+    // g.addEdge(0,3);
+    // g.addEdge(1,2);
+    // g.addEdge(1,3);
+    // g.addEdge(2,3);
+    // g.addEdge(2,4);
+    // g.addEdge(3,4);
+    // g.addEdge(3,5);
+    // g.addEdge(4,5);
+    // g.topologicalSort_DFS();
+    // cout<<endl;
+    Graph g1(6);
+    g1.addEdge(2,3);
+    g1.addEdge(3,1);
+    g1.addEdge(4,0);
+    g1.addEdge(4,1);
+    g1.addEdge(5,0);
+    g1.addEdge(5,2);
+    g1.topologicalSort_DFS();
+    cout<<endl;
+    g1.topologicalSort_BFS();
     
     return 0;
 
